3b01a5663ca9ff7f2a63d5d83490ca61bd44ea39
ov <ov@ov.ov>
2017-11-20 16:20:59 +0530

    First commit

---
+++ CONFIG/config.php
@@ -0,0 +1,10 @@
+<?php
+//$conn = new mysqli('localhost', 'root', '0000', 'suv_barta');
+//if ($conn->connect_error) {    die("Connection failed: " . $conn->connect_error);}
+//$conn->query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
+//mysqli_set_charset($conn,"utf8");
+$db = "suvankar_barta";
+$host="localhost";
+$user="suvankar_barta";
+$pass = "asdqwe";
+$table1= "news";
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+define("APP_PATH", getcwd());
+define("APP_PATH", getcwd());
+include('Diz/APP/ENGINE.php');
+//echo $lnk;
+ ?>
