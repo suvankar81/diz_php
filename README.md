1950cb8c3fb490c3611e74d17406b0ebfd0f380c
ov <ov@ov.ov>
2017-11-20 19:34:45 +0530

    started

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # diz_php
 PHP - the Simple and organized my_way
+i follow this structure for general purpose PHP projects.
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 define("APP_PATH", getcwd());
-define("APP_PATH", getcwd());
+define("HTTP_HOST", "");//$_SERVER['HTTP_HOST'] == 'localhost:8080';$_SERVER['SERVER_NAME'] == 'localhost';
+define("SERVER_NAME","");
 include('Diz/APP/ENGINE.php');
 //echo $lnk;
  ?>
