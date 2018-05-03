d92115dc6274e744f1552fe025e769b3e73eead3
the-no-one <sarkar.suvankar@gmail.com>
2018-05-03 21:46:19 +0530

    Update index.php

--- CONTENT/ROOT_URI/index.php
+++ CONTENT/ROOT_URI/index.php
@@ -1,7 +1,10 @@
 <?php
-//echo $lnk;
-if($lnk=="") include "en-bn.php";
-elseif($lnk=="EN") include "EN_HOME.php";
-elseif($lnk=="BN") include "BN_HOME.php";
-else echo "404";
+$lnk2 = explode('?', $lnk);
+  include("HEADER.php");// echo "<br>";var_dump($_SERVER['HTTP_REFERER']);// echo $lnk."c";
+  if($lnk=="") include("home.php");
+  elseif(file_exists(__DIR__."/".$lnk.".php"))  include($lnk.".php");
+  elseif(isset($lnk2[1]) && file_exists(__DIR__."/".$lnk2[0].".php") ) include($lnk2[0].".php");
+  else include("404.php");
+  include("FOOTER.php");
+
 ?>
